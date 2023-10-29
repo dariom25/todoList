619d59e0dc3892a7b20443b974790dd40d975b33
dariom25 <dario@maciey.de>
2023-10-29 21:30:42 +0100

    Add showDialog method

--- src/view/view.js
+++ src/view/view.js
@@ -1,6 +1,8 @@
+import { createDialog } from "./components/todo-dialog";
+
 export class View {
     constructor() {
-
+        //erstelle hier das display
     }
 
     render(todo) {
@@ -8,4 +10,28 @@ export class View {
             console.log(todo[property]);
         }
     }
+
+    //method to add eventListeners
+
+    createElement(tag, className) {
+        const element = document.createElement(tag)
+        if (className) element.classList.add(className)
+    
+        return element
+      }
+    
+      getElement(selector) {
+        const element = document.querySelector(selector)
+    
+        return element
+      }
+
+    showDialog() {
+        const dialog = createDialog();
+        const content = document.querySelector("#content");
+        content.appendChild(dialog);
+        dialog.showModal();
+        
+    }
+
 }
