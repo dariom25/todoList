ac663ca113caed725b4526cefb0d4e7b1b2910f4
dariom25 <dario@maciey.de>
2023-11-01 11:03:17 +0100

    Bind events

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,6 +1,18 @@
-class Controller {
-    constructor(model, view) {
-        this.model = model;
+import { Todo } from "../model/todo";
+import { TodoList } from "../model/todo-list";
+import { View } from "../view/view";
+
+export class Controller {
+    constructor(todoListModel, view) {
+        this.todoListModel = todoListModel;
         this.view = view;
+        this.bindEvents();
+    }
+
+
+
+    bindEvents() {
+        this.view.bindShowTodoDialog();
     }
 }
+
