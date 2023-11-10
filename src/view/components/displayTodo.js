7c87b6e89871b2600981f136749381645a889321
dariom25 <dario@maciey.de>
2023-11-10 17:34:54 +0100

    Add parameters and content

--- src/view/components/displayTodo.js
+++ src/view/components/displayTodo.js
@@ -1,4 +1,4 @@
-export function displayTodo() {//argumente fehlen hier noch und textContent und so muss noch eingefügt werden
+export function displayTodo(title, description, dueDate, category, priority) {
     const content = document.querySelector("#content");
     const todoContainer = document.createElement("div");
     todoContainer.setAttribute("class", "todo-container");
@@ -8,25 +8,30 @@ export function displayTodo() {//argumente fehlen hier noch und textContent und 
 
     const todoTitle = document.createElement("div");
     todoTitle.setAttribute("class", "todo-title");
+    todoTitle.textContent = title;
     todo.appendChild(todoTitle);
 
     const todoDescription = document.createElement("div");
-    todoDescription.setAttribute("class", "tood-description");
+    todoDescription.setAttribute("class", "todo-description");
+    todoDescription.textContent = description;
     todo.appendChild(todoDescription);
 
     const todoDueDate = document.createElement("div");
     todoDueDate.setAttribute("class", "todo-due-date");
+    todoDueDate.textContent = dueDate;
     todo.appendChild(todoDueDate);
 
     const todoCategory = document.createElement("div");
     todoCategory.setAttribute("class", "todo-category");
+    todoCategory.textContent = category;
     todo.appendChild(todoCategory);
 
     const todoPriority = document.createElement("div");
     todoPriority.setAttribute("class", "todo-prioriy");
+    todoPriority.textContent = priority;
     todo.appendChild(todoPriority);
 
     const todoComplete = document.createElement("input");
-    todoComplete.type = "checkbox";
+    todoComplete.type = "checkbox"; //hier muss vielleicht nochmal geprüft werden, ob das alles korrekt angezeigt wird
     todo.appendChild(todoComplete);
 }
