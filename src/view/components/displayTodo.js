50408fbbc0a74a0b10f594e4045650697e2e6f10
dariom25 <dario@maciey.de>
2023-11-11 17:31:06 +0100

    Fix namingbug

--- src/view/components/displayTodo.js
+++ src/view/components/displayTodo.js
@@ -1,34 +1,34 @@
 export function displayTodo(title, description, dueDate, category, priority) {
     const todosContainer = document.querySelector(".todos");
-    const todoContainer = document.querySelector(".todoElement-container");
+    const todoContainer = document.querySelector(".todo-container");
     todosContainer.appendChild(todoContainer);
     
     const todoElement = document.createElement("div");
-    todoElement.setAttribute("class", "todoElement-element");
+    todoElement.setAttribute("class", "todo-element");
     todoContainer.appendChild(todoElement);
 
     const todoTitle = document.createElement("div");
-    todoTitle.setAttribute("class", "todoElement-title");
+    todoTitle.setAttribute("class", "todo-title");
     todoTitle.textContent = title;
     todoElement.appendChild(todoTitle);
 
     const todoDescription = document.createElement("div");
-    todoDescription.setAttribute("class", "todoElement-description");
+    todoDescription.setAttribute("class", "todo-description");
     todoDescription.textContent = description;
     todoElement.appendChild(todoDescription);
 
     const todoDueDate = document.createElement("div");
-    todoDueDate.setAttribute("class", "todoElement-due-date");
+    todoDueDate.setAttribute("class", "todo-due-date");
     todoDueDate.textContent = dueDate;
     todoElement.appendChild(todoDueDate);
 
     const todoCategory = document.createElement("div");
-    todoCategory.setAttribute("class", "todoElement-category");
+    todoCategory.setAttribute("class", "todo-category");
     todoCategory.textContent = category;
     todoElement.appendChild(todoCategory);
 
     const todoPriority = document.createElement("div");
-    todoPriority.setAttribute("class", "todoElement-priority");
+    todoPriority.setAttribute("class", "todo-priority");
     todoPriority.textContent = priority;
     todoElement.appendChild(todoPriority);
 
