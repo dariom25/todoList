e40cb40a7063defe174b1c59772d0d0d3e10bb2f
dariom25 <dario@maciey.de>
2023-11-11 17:29:21 +0100

    Change naming for better understanding

--- src/view/components/displayTodo.js
+++ src/view/components/displayTodo.js
@@ -1,39 +1,38 @@
 export function displayTodo(title, description, dueDate, category, priority) {
     const todosContainer = document.querySelector(".todos");
-    const todoContainer = document.createElement("div");
-    todoContainer.setAttribute("class", "todo-container");
+    const todoContainer = document.querySelector(".todoElement-container");
     todosContainer.appendChild(todoContainer);
     
-    const todo = document.createElement("div");
-    todo.setAttribute("class", "todo-element");
-    todoContainer.appendChild(todo);
+    const todoElement = document.createElement("div");
+    todoElement.setAttribute("class", "todoElement-element");
+    todoContainer.appendChild(todoElement);
 
     const todoTitle = document.createElement("div");
-    todoTitle.setAttribute("class", "todo-title");
+    todoTitle.setAttribute("class", "todoElement-title");
     todoTitle.textContent = title;
-    todo.appendChild(todoTitle);
+    todoElement.appendChild(todoTitle);
 
     const todoDescription = document.createElement("div");
-    todoDescription.setAttribute("class", "todo-description");
+    todoDescription.setAttribute("class", "todoElement-description");
     todoDescription.textContent = description;
-    todo.appendChild(todoDescription);
+    todoElement.appendChild(todoDescription);
 
     const todoDueDate = document.createElement("div");
-    todoDueDate.setAttribute("class", "todo-due-date");
+    todoDueDate.setAttribute("class", "todoElement-due-date");
     todoDueDate.textContent = dueDate;
-    todo.appendChild(todoDueDate);
+    todoElement.appendChild(todoDueDate);
 
     const todoCategory = document.createElement("div");
-    todoCategory.setAttribute("class", "todo-category");
+    todoCategory.setAttribute("class", "todoElement-category");
     todoCategory.textContent = category;
-    todo.appendChild(todoCategory);
+    todoElement.appendChild(todoCategory);
 
     const todoPriority = document.createElement("div");
-    todoPriority.setAttribute("class", "todo-priority");
+    todoPriority.setAttribute("class", "todoElement-priority");
     todoPriority.textContent = priority;
-    todo.appendChild(todoPriority);
+    todoElement.appendChild(todoPriority);
 
     const todoComplete = document.createElement("input");
     todoComplete.type = "checkbox"; //hier muss vielleicht nochmal geprüft werden, ob das alles korrekt angezeigt wird
-    todo.appendChild(todoComplete);
+    todoElement.appendChild(todoComplete);
 }
