4100279b03275f751c1646b4a31c517b59977644
dariom25 <dario@maciey.de>
2023-11-13 19:30:01 +0100

    Add image management with wepback

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpeg|gif)$/i,
+        type: "asset/ressource",
+      },
     ],
   },
 };
