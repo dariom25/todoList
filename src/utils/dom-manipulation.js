b7b1e8dcfb6d3eacbb2374ab9c53ea4b9c8afc38
dariom25 <dario@maciey.de>
2023-11-22 18:09:02 +0100

    Initial dist subtree commit

--- src/utils/dom-manipulation.js
+++
@@ -1,7 +0,0 @@
-export function createTextElement(text = "default text", selector = "class", selectorName = "default-class") {
-    const textElement = document.createElement("div");
-    textElement.textContent = `${text}`;
-    textElement.setAttribute(selector, selectorName);
-
-    return textElement;    
-}
